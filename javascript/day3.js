b46de525dfd5d083c5d4a4f11da4323e44f7e353
Paige <paigemacgregor33@gmail.com>
2019-12-04 10:11:06 -0800

    day3 part1 returning matching values

--- javascript/day3.js
+++ javascript/day3.js
@@ -1,8 +1,8 @@
-//const Path1 = ['R8','U5','L5','D3']
-//const Path2 = ['U7','R6','D4','L4']
+const Path1 = ['R8','U5','L5','D3']
+const Path2 = ['U7','R6','D4','L4']
 
-const Path1 = ['R75','D30','R83','U83','L12','D49','R71','U7','L72']
-const Path2 = ['U62','R66','U55','R34','D71','R55','D58','R83']
+//const Path1 = ['R75','D30','R83','U83','L12','D49','R71','U7','L72']
+//const Path2 = ['U62','R66','U55','R34','D71','R55','D58','R83']
 
 convertPaths = (path) => {
     let newPath = [];
@@ -34,9 +34,38 @@ convertPaths = (path) => {
                 finalPath.push([moves,0])
             }
     }
-    console.log(finalPath);
+    return finalPath
 }
-3, 2 - 2, 3
 
-convertPaths(Path1)
-convertPaths(Path2)
+
+let convertedPath1 = convertPaths(Path1)
+let convertedPath2 = convertPaths(Path2)
+
+//console.log(convertedPath1);
+//console.log(convertedPath2);
+
+getDistance = (path) => {
+    let distanceArray = [];
+    let distance = 0;
+    for(let i = 0; i < path.length; i++){
+        distance = distance + (Math.abs(path[i][0]) + Math.abs(path[i][1]))
+        distanceArray.push(distance);
+    }
+    return distanceArray;
+}
+
+let distance1 = getDistance(convertedPath1);
+let distance2 = getDistance(convertedPath2);
+
+getSame = (dist1,dist2) => {
+    for(let i = 0; i < dist1.length; i++){
+        if(dist1[i] === dist2[i]){
+            stuff
+        }
+    }
+}
+
+getSame(distance1,distance2)
+
+
+
